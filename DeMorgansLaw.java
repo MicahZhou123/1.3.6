d43219b7cc651edf343a5e707dc90204020f53d0
MicahZhou123 <30tbit@gmail.com>
2025-10-24 14:21:35 -0700

    Add files via upload

---
+++ DeMorgansLaw.java
@@ -0,0 +1,62 @@
+/*
+ * Activity 1.3.6
+ * Validates both parts of De Morgan's Laws.
+ */
+import java.util.Scanner;
+
+public class DeMorgansLaw
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 'true' or 'false' for A");
+        boolean a = sc.nextBoolean();
+
+        System.out.println("Enter 'true' or 'false' for B");
+        boolean b = sc.nextBoolean();
+
+        System.out.println("--- De Morgan's Law 1 ---");
+
+        if (!(a && b))
+        {
+            System.out.println("!(a && b): TRUE");
+        }
+        else
+        {
+            System.out.println("!(a && b): FALSE");
+        }
+
+        if (!a || !b)
+        {
+            System.out.println("(!a || !b): TRUE");
+        }
+        else
+        {
+            System.out.println("(!a || !b): FALSE");
+        }
+
+
+        System.out.println("--- De Morgan's Law 2 ---");
+
+        if (!(a || b))
+        {
+            System.out.println("!(a || b): TRUE");
+        }
+        else
+        {
+            System.out.println("!(a || b): FALSE");
+        }
+
+        if (!a && !b)
+        {
+            System.out.println("(!a && !b): TRUE");
+        }
+        else
+        {
+            System.out.println("(!a && !b): FALSE");
+        }
+
+        sc.close();
+    }
+}
